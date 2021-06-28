866cf0685f279ad1191aa5acaf3462f653b864c4
MikDR2000 <s1095953@studenti.univpm.it>
2021-06-28 15:41:30 +0200

    Aggiunti commenti

--- calendario/Calendario.py
+++ calendario/Calendario.py
@@ -17,9 +17,11 @@ class Calendario(QWidget):
         self.setLayout(self.vbox)
         self.show()
 
+    #funzione che crea il calendario concretamente
     def Calendar(self):
         self.vbox = QVBoxLayout()
         self.calendario = QCalendarWidget()
+        #settiamo a visibile la griglia nel calendario per facilitare la distinzione dei giorni
         self.calendario.setGridVisible(True)
         self.calendario.clicked.connect(self.data_selezionata)
         self.vbox.addWidget(self.calendario)
@@ -32,12 +34,16 @@ class Calendario(QWidget):
         btn_esc.clicked.connect(self.funz_esci)
         self.vbox.addWidget(btn_esc)
 
+    #questa funzione acquisisce e salva la data che andiamo a selezionare (clickare) sul calendario, la formatta in un
+    #formato a noi comodo e la salva in un ulteriore variabile che andremo a passare nel costruttore VistaListaPrenotazioni
+    #per far vedere la lista delle prenotazioni di quel giorno
     def data_selezionata(self):
         data_selezionata = self.calendario.selectedDate()
         data_da_usare = "{}/{}/{}".format(data_selezionata.day(), data_selezionata.month(), data_selezionata.year())
         self.vista_lista_prenotazioni = VistaListaPrenotazioni(data_da_usare)
         self.vista_lista_prenotazioni.show()
 
+    #questa funzione apre l'interfaccia per l'inserimento di una nuova prenotazione
     def show_new_prenotazione(self):
         self.vista_inserisci_prenotazione = VistaInserisciPrenotazione(self.controller, self.update_ui)
         self.vista_inserisci_prenotazione.show()
@@ -45,6 +51,7 @@ class Calendario(QWidget):
     def update_ui(self):
         pass
 
+    #chiude la nostra pagina con il calendario
     def funz_esci(self):
         from home.views.VistaHome import VistaHome
         self.vista_home = VistaHome()
